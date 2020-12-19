19fb192de8fbd81a2a967d4bdc7825b7dc35ca4a
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2020-12-19 15:18:52 +0900

    Eye Bezier
    bezier test

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
 }
 
 function draw() {
-  background(220);
+  noFill();
+  stroke(255, 102, 0);
+  line(85, 20, 10, 10);
+  line(90, 90, 15, 80);
+  stroke(0, 0, 0);
+  bezier(width/2 - 300, height/2, width/2-160, height/2-200, width/2+80, height/2-200, width/2 + 390, height/2);
+  bezier(width/2 - 300, height/2+30, width/2-160, height/2-200+50, width/2+80, height/2-200+50, width/2 + 400, height/2+60);
+  bezier(width/2 - 300, height/2+50, width/2-160, height/2+170, width/2+190, height/2+170, width/2 + 400, height/2+80);
 }
