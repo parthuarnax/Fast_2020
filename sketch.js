712ff90f46acf55da06053ed41d446a414a972e0
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2020-12-19 22:01:53 +0900

    수정사항 반영
    과제 제출하기 전까지 눈 못 감게 하기 ; 마우스로 눈 떴다 감았다 하기는 없애기로 한다
    과제 몇 개 제출했는지 확인할 수 있도록 띄워주기
    과제 제출 이펙트 만들기

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let assignment_arr = new Array("맞습니다", "네", "감사합니다", "안녕
 let countdown = 10;
 let picked_assignment = 2;
 let completed_assignment = 0;
-
+let prev_assignment = picked_assignment;
 let hangang;
 
 function setup() {
@@ -25,7 +25,6 @@ function setup() {
   bezier_val[11] = width/2 + 190;
   bezier_val[12] = height/2 + 170;
   bezier_val[13] = height/2 + 60;
-
   hangang = loadImage('assets/hangang.png');
 
   input = createInput();
@@ -42,40 +41,41 @@ function draw() {
   fill(255);
   
   if(assignment == assignment_arr[picked_assignment] && completed_assignment < 5){ // 과제 제출에 성공 && 과제 제출량이 5미만이면 다음 과제를 낸다.
-    picked_assignment = int(random(0, 13));
+    if(prev_assignment != 0 && prev_assignment != 13){
+      picked_assignment = random(0,1)<0.5 ? int(random(0, prev_assignment)) : int(random(prev_assignment, 13)) ;
+    } else {
+      picked_assignment = int(random(1,12));
+    }
+    prev_assignment = picked_assignment;
     completed_assignment++;
     countdown = 10;
-  }
-
-  if(completed_assignment == 5){ // 과제 제출량 초기화와 카운트다운 추가
-    completed_assignment = 0;
-    countdown = 10;
+    if(completed_assignment == 4){
+      week++;
+    }
   }
 
   text('"'+assignment_arr[picked_assignment]+'"'+"라고 말하세요."+'\n'+"제출기한은 "+countdown+"초 남았습니다.", width/4.5, height/8);
+  textSize(25);
+  text("제출한 과제 수 : "+completed_assignment, width/2 - 120, height - 170);
   stroke(255);
-  let clickState = 1;
 
   noFill();
-  switch(clickState){
-    case -1 :
-      lerpChange();
-      strokeWeight(8);
-      bezier(width/2 - 380, bezier_val[0], width/2-200, bezier_val[1], width/2+80, bezier_val[2], width/2 + 365, bezier_val[3]);
-      strokeWeight(10);
-      bezier(width/2 - 400, height/2+30, bezier_val[4], bezier_val[5], bezier_val[6], bezier_val[7], width/2 + 400, height/2 + 20);
-      bezier(width/2 - 400, bezier_val[8], bezier_val[9], bezier_val[10], bezier_val[11], bezier_val[12], width/2 + 400, bezier_val[13]);
-      break;
-    case 1 :
-      lerpChange();
-      strokeWeight(7);
-      bezier(width/2 - 380, bezier_val[0], width/2-200, bezier_val[1], width/2+80, bezier_val[2], width/2 + 365, bezier_val[3]);
-      strokeWeight(10);
-      bezier(width/2 - 400, height/2+30, bezier_val[4], bezier_val[5], bezier_val[6], bezier_val[7], width/2 + 400, height/2 + 20);
-      bezier(width/2 - 400, bezier_val[8], bezier_val[9], bezier_val[10], bezier_val[11], bezier_val[12], width/2 + 400, bezier_val[13]);
 
-      break;
+  if(completed_assignment == 5){
+    background(0);
+    strokeWeight(0);
+    fill(255);
+    textSize(40);
+    text("5초간 휴식, 현재 "+week+" 주차", width/3.6, height/6);
+    countdown = 10;
   }
+  
+  eyeStateChange();
+  strokeWeight(8);
+  bezier(width/2 - 380, bezier_val[0], width/2-200, bezier_val[1], width/2+80, bezier_val[2], width/2 + 365, bezier_val[3]);
+  strokeWeight(10);
+  bezier(width/2 - 400, height/2+30, bezier_val[4], bezier_val[5], bezier_val[6], bezier_val[7], width/2 + 400, height/2 + 20);
+  bezier(width/2 - 400, bezier_val[8], bezier_val[9], bezier_val[10], bezier_val[11], bezier_val[12], width/2 + 400, bezier_val[13]);
 
   if(frameCount%80 == 0){
     countdown--;
@@ -85,6 +85,25 @@ function draw() {
     }
   }
 
+  if(completed_assignment > temp_assignment){
+    effect_bool = true;
+  }
+
+  if(effect_bool){
+    fill_val = lerp(fill_val, 0, 0.09);
+    noStroke();
+    fill(255, fill_val);
+    rect(0,0,width,height);
+    if(fill_val < 3){
+      fill_val = 0;
+      effect_bool = false;
+    }
+  } else {
+    fill_val = 255;
+  }
+  
+
+
   if(gameOver){
     background(0, 20);
     text(assignment, width/2, height/2);
@@ -93,10 +112,16 @@ function draw() {
     button.mousePressed(struggle);
     image(hangang,0,0);
   }
+
+  temp_assignment = completed_assignment;
 }
 
+let week = 0;
+let fill_val = 255;
+let effect_bool = false;
+let temp_assignment = 0;
 let gameOver = false;
-let changeSpd = 0.07;
+let changeSpd = 0.02;
 
 function assignment_check(){
   if(assignment == assignment_arr[picked_assignment]){
@@ -104,8 +129,8 @@ function assignment_check(){
   }
 }
 
-function lerpChange(){
-  if(clickState == 1){
+function eyeStateChange(){
+  if(completed_assignment != 5){
     bezier_val[0] = lerp(bezier_val[0], height/2 - 60, changeSpd);
     bezier_val[1] = lerp(bezier_val[1], height/2 - 250, changeSpd); 
     bezier_val[2] = lerp(bezier_val[2], height/2 - 200, changeSpd); 
@@ -120,7 +145,7 @@ function lerpChange(){
     bezier_val[11] = lerp(bezier_val[11], width/2 + 190, changeSpd); 
     bezier_val[12] = lerp(bezier_val[12], height/2 + 170, changeSpd); 
     bezier_val[13] = lerp(bezier_val[13], height/2 + 60, changeSpd); 
-  } else if(clickState == -1) {
+  } else if(completed_assignment == 5) {
     bezier_val[0] = lerp(bezier_val[0], height/2 - 10, changeSpd);
     bezier_val[1] = lerp(bezier_val[1], height/2 + 30, changeSpd); 
     bezier_val[2] = lerp(bezier_val[2], height/2 + 50, changeSpd); 
@@ -141,9 +166,5 @@ function lerpChange(){
 let assignment;
 function struggle(){
   assignment = input.value();
-}
-
-function mouseReleased(){
-  clickState *= -1;
 }
 
